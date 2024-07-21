c56c5404bb07a4df19083affcb04fb6abdcab1e4
Toivo Snåre <topisn@gmail.com>
2024-07-21 18:27:22 +0300

    Reimplement mm.Region with new page allocator interface

--- kernel/src/mm/Region.zig
+++ kernel/src/mm/Region.zig
@@ -1,16 +1,13 @@
 const mm = @import("../mm.zig");
-const PageFramePtr = mm.PageFramePtr;
-const PageFrameSlice = mm.PageFrameSlice;
+const PagePtr = mm.PagePtr;
+const PageSlice = mm.PageSlice;
 const Region = @This();
 
 // Ref count of zero implies that the region is not in use.
 ref_count: usize,
-page_frame_count: usize,
-page_frames: [MAX_PAGE_FRAMES]PageFramePtr,
+pages: PageSlice,
 
-const MAX_PAGE_FRAMES = 16;
 const MAX_REGIONS = 128;
-
 var regions: [MAX_REGIONS]Region = undefined;
 
 pub fn init() void {
@@ -19,11 +16,11 @@ pub fn init() void {
     }
 }
 
-pub fn allocate() !*Region {
+pub fn allocate(order: usize) !*Region {
     for (&regions) |*r| {
         if (r.isFree()) {
+            r.pages = try mm.page_allocator.allocate(order);
             r.ref_count = 1;
-            r.page_frame_count = 0;
             return r;
         }
     }
@@ -31,23 +28,10 @@ pub fn allocate() !*Region {
 }
 
 pub fn free(self: *Region) void {
+    mm.page_allocator.free(self.pages);
     self.ref_count = 0;
 }
 
 pub fn isFree(self: Region) bool {
     return self.ref_count == 0;
-}
-
-pub fn addPageFrame(self: *Region, page_frame: PageFramePtr) !void {
-    if (self.page_frame_count == MAX_PAGE_FRAMES)
-        return error.PageFrameTableFull;
-    self.page_frames[self.page_frame_count] = page_frame;
-    self.page_frame_count += 1;
-}
-
-pub fn addPageFrames(self: *Region, page_frames: PageFrameSlice) !void {
-    if (self.page_frame_count + page_frames.len > MAX_PAGE_FRAMES)
-        return error.PageFrameTableFull;
-    for (page_frames) |*pf|
-        try self.addPageFrame(pf);
 }
