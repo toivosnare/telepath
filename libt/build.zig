d40d6fab3643f3397fa005e34c31cf8bc07486b0
Toivo Snåre <topisn@gmail.com>
2024-09-03 15:21:04 +0300

    Add _start routine to libt

--- libt/build.zig
+++ libt/build.zig
@@ -1,7 +1,19 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("libt", .{
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const include_entry_point = b.option(bool, "include_entry_point", "") orelse false;
+    const stack_size = b.option(u32, "stack_size", "") orelse 0x10;
+    const options = b.addOptions();
+    options.addOption(bool, "include_entry_point", include_entry_point);
+    options.addOption(usize, "stack_size", stack_size);
+
+    const module = b.addModule("libt", .{
+        .target = target,
+        .optimize = optimize,
         .root_source_file = b.path("libt.zig"),
     });
+    module.addOptions("options", options);
 }
--- libt/libt.zig
+++ libt/libt.zig
@@ -1,2 +1,44 @@
+const std = @import("std");
+const root = @import("root");
+const options = @import("options");
+
 pub const syscall = @import("syscall.zig");
 pub const tix = @import("tix.zig");
+
+comptime {
+    if (options.include_entry_point)
+        @export(_start, .{ .name = "_start" });
+}
+
+fn _start() callconv(.Naked) noreturn {
+    const max_virtual = 0x3FFFFFFFFF;
+    // Allocate and map stack, jump to main.
+    asm volatile (
+        \\li a0, %[allocate_id]
+        \\li a1, %[stack_size]
+        \\li a2, %[stack_permissions]
+        \\li a3, 0
+        \\ecall
+        \\bltz a0, 1f
+        \\mv a1, a0
+        \\li a0, %[map_id]
+        \\mv a2, %[virtual_address]
+        \\ecall
+        \\bltz a0, 1f
+        \\mv sp, %[stack_address]
+        \\jr %[main]
+        \\1:
+        \\li a0, %[exit_id]
+        \\li a1, 1
+        \\ecall
+        :
+        : [allocate_id] "I" (@intFromEnum(syscall.Id.allocate)),
+          [stack_size] "I" (options.stack_size),
+          [stack_permissions] "I" (syscall.Permissions{ .readable = true, .writable = true }),
+          [map_id] "I" (@intFromEnum(syscall.Id.map)),
+          [virtual_address] "{t0}" (max_virtual - options.stack_size * std.mem.page_size),
+          [stack_address] "{t1}" (max_virtual),
+          [main] "{t2}" (&root.main),
+          [exit_id] "I" (@intFromEnum(syscall.Id.exit)),
+    );
+}
